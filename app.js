7dd18fe9aaaabedde246ecc3d9f5e4df7ff4b22f
David Tang <dtang@auditboard.com>
2019-10-18 20:23:24 -0700

    remove cors

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
 
 app.use(express.json());
-app.use(cors({
-  // origin: 'https://www.google.com'
-  // origin: 'http://localhost:3000'
-}));
 
 const db = {
   posts: [
